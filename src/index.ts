79fac4625c6be8b28ebbd5c9b1f4bccf0e22e712
Matin Azizi <95720847+MataAzi@users.noreply.github.com>
2022-02-16 15:01:31 +0330

    Fix Exports & Dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import 'reflect-metadata';
 import NodePay from './base/NodePay';
+import MemoryStorage from './storages/Memory';
+import IdPayProvider from './providers/IDPayProvider';
 
+export { MemoryStorage, IdPayProvider }
 export default NodePay;
